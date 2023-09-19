dd34de728be773e2d7368d7d74473d64b3a23c95
YanielRamos <yanielramos0504@gmail.com>
2023-09-19 19:25:17 -0400

    ok

--- javascript-web_scraping/1-writeme.js
+++ javascript-web_scraping/1-writeme.js
@@ -2,11 +2,22 @@
 /* writes a string to a file */
 const fs = require('fs');
 
-// contain name of the file
-fs.writeFile(process.argv[2], process.argv[3], 'utf-8', (err) => {
+// Check if the correct number of command-line arguments is provided
+if (process.argv.length !== 4) {
+  console.error('Usage: node 1-writeme.js <file_path> <string_to_write>');
+  process.exit(1);
+}
+
+// Get the file path and string to write from the command-line arguments
+const filePath = process.argv[2];
+const stringToWrite = process.argv[3];
+
+// Write the string to the file in utf-8 encoding
+fs.writeFile(filePath, stringToWrite, 'utf-8', (err) => {
   if (err) {
-    console.log(err);
+    console.error(err);
   } else {
-    console.log(process.argv[3]);
+    console.log(stringToWrite);
   }
 });
+
